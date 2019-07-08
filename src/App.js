9646c5cf7939caf445a7656dc726889fd5845799
evgeni <evgeni.pirianov@level01.io>
2019-07-08 23:48:14 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye React Kit :)!!!
+          On the new branch Feature!
         </p>
         <a
           className="App-link"
